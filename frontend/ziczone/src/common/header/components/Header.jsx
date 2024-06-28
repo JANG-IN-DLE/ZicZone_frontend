a5623805ff783e66b7e98590cead9ef420b21c41
kangseunggyuu <157556960+kangseunggyuu@users.noreply.github.com>
2024-06-28 18:41:49 +0900

    헤더 수정 완료

--- frontend/ziczone/src/common/header/components/Header.jsx
+++ frontend/ziczone/src/common/header/components/Header.jsx
@@ -1,44 +1,47 @@
 import React from "react";
-import { BrowserRouter, Router, Routes, Link } from "react-router-dom";
-import header from '../assets/Header.png'
-import './../styles/Header.css'
+import { BrowserRouter, Router, Routes, Link } from "react-router-dom"; // 리액트 라우터의 Link 컴포넌트 import
+import header from '../assets/Header.png';
+import './../styles/Header.css';
 import NonLogin from "./NonLogin";
 import UserLogin from "./UserLogin";
 import CompLogin from "./CompLogin";
 
 const Header = () => {
-
     return (
         <BrowserRouter>
-            <header>
+            <div className="header">
                 <nav className="header_nav">
-                    <ul className="header_nav_list">
-                        <div className="header_logo">
-                            <Link to="/"><img src={header} alt="Logo" /></Link>
-                        </div>
+                    <div className="header_logo">
+                        <Link to="/">
+                            <img src={header} alt="Logo" />
+                        </Link>
+                    </div>
+                    <div className="header_nav_list">
                         <div className="header_pick_zone">
-                            <li>
+                            <div>
                                 <Link to="/">PICK존</Link>
-                            </li>
+                            </div>
                         </div>
                         <div className="header_help_zone">
-                            <li>
+                            <div>
                                 <Link to="/">HELP존</Link>
-                            </li>
+                            </div>
                         </div>
                         <div className="header_company_zone">
-                            <li>
+                            <div>
                                 <Link to="/">COMPANY존</Link>
-                            </li>
+                            </div>
                         </div>
-                    </ul>
-                    <NonLogin />
-                    {/* <UserLogin /> */}
-                    {/* <CompLogin /> */}
+                    </div>
+                    <div className="non_login_div">
+                        <NonLogin />
+                        {/* <UserLogin /> */}
+                        {/* <CompLogin /> */}
+                    </div>
                 </nav>
-            </header>
+            </div>
         </BrowserRouter>
-    )
+    );
 }
 
 export default Header;
--- frontend/ziczone/src/common/header/components/UserLogin.jsx
+++ frontend/ziczone/src/common/header/components/UserLogin.jsx
@@ -3,6 +3,7 @@ import './../styles/UserLogin.css'
 import dropdown from '../assets/Dropdown.png'
 import alarm from'../assets/Alarm.png'
 import useDropdown from "../hooks/useDropdown";
+import { Link } from "react-router-dom";
 
 const UserLogin = () => {
     const {open, Dropdown} = useDropdown();
@@ -14,10 +15,10 @@ const UserLogin = () => {
             </div>
             <div className="user_login_name">
                 강승규님
-                <ul className={`dropdown_list ${open ? "show" : ""}`}>
-                    <div><li>마이페이지</li></div>
-                    <div><li>로그아웃</li></div>
-                </ul>
+                <div className={`dropdown_list ${open ? "show" : ""}`}>
+                    <div><Link to="/">마이페이지</Link></div>
+                    <div><Link to="/">로그아웃</Link></div>
+                </div>
             </div>
             <div className="dropdown" onClick={Dropdown}>
                     <img src={dropdown} alt="Dropdown" />
