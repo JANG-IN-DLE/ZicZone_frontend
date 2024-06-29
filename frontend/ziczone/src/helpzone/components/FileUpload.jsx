6f756ffaebd64e0d22debc60d5e404ed8c5872b8
CHAERIM LEE <leechaerim214@gmail.com>
2024-06-29 17:31:12 +0900

    feat: FileUpload 메소드 추가

--- frontend/ziczone/src/helpzone/components/FileUpload.jsx
+++ frontend/ziczone/src/helpzone/components/FileUpload.jsx
@@ -2,8 +2,18 @@ import React from "react";
 import useFileUpload from "../hooks/useFileUpload.js";
 import "../styles/FileUpload.css";
 
-const FileUpload = () => {
+const FileUpload = ({ onFileChange }) => {
     const { file, handleFileChange, handleFileRemove } = useFileUpload();
+
+    const onChange = (event) => {
+        handleFileChange(event);
+        onFileChange(event.target.files[0]);
+    };
+
+    const onRemove = () => {
+        handleFileRemove();
+        onFileChange(null);
+    };
 
     return (
         <div className="file_upload">
@@ -12,12 +22,12 @@ const FileUpload = () => {
                     type="file"
                     id="pdf_input"
                     accept=".pdf"
-                    onChange={ handleFileChange }
+                    onChange={ onChange }
                     style={{ display: "none" }}
                 />
                 <div className="fu_display">
                     {file ? file.name : "PDF 파일을 선택해주세요"}
-                    {file && <span className="fu_remove" onClick={ handleFileRemove }>X</span>}
+                    {file && <span className="fu_remove" onClick={ onRemove }>X</span>}
                 </div>
             </label>
         </div>
