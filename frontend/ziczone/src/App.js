0786f16874d3aaf5f4570063f9ff66fd88f883ba
CHAERIM LEE <leechaerim214@gmail.com>
2024-07-01 11:54:28 +0900

    test: 컴포넌트 확인

--- frontend/ziczone/src/App.js
+++ frontend/ziczone/src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
-// import './App.css';
-import MainComponent from './main/components/MainComponent';
-import Pickzone from './pickzone/components/Pickzone';
-import LoginMainComponent from './main/components/LoginMainComponent';
+// import logo from './logo.svg';
+// // import './App.css';
+// import MainComponent from './main/components/MainComponent';
+// import Pickzone from './pickzone/components/Pickzone';
+// import LoginMainComponent from './main/components/LoginMainComponent';
+import CUBoard from "./helpzone/components/CUBoard";
 
 function App() {
   return (
     <div>
-      <Pickzone />    
-      <LoginMainComponent/>
+      <CUBoard />
     </div>
   );
 }
