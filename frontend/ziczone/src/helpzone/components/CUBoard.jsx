97ee8c7fa0fb113e8907452514346760ac1da91a
CHAERIM LEE <leechaerim214@gmail.com>
2024-07-01 11:52:23 +0900

    feat: 수정 모드

--- frontend/ziczone/src/helpzone/components/CUBoard.jsx
+++ frontend/ziczone/src/helpzone/components/CUBoard.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../common/header/components/Header";
 import "../styles/CUBoard.css";
 import ProfileCard from "./ProfileCard";
 import Description from "./Description";
-import PostFrom from "./PostForm";
+import PostForm from "./PostForm";
 
-const Board = () => {
+const CUBoard = () => {
+  const [isEditMode, setIsEditMode] = useState(true);
+
   const userProfile = {
     jobs: ['게임 클라이언트', 'devops/시스템', '게임클라이언트'],
     gender: 'female',
@@ -16,32 +18,39 @@ const Board = () => {
     stacks: ['Python', 'NativeScript', 'Spring Boot', 'Microsoft SQL Server', 'MySQL', 'MySQL', 'MyhSQL']
   };
 
+  const initialData = {
+    berry: 300,
+    title: '자소서 첨삭 요청',
+    content: '경력사항에 아르바이트 적어도 되나요?',
+    file: null
+  };
+
   return (
     <div>
       <Header />
       <div className="b_section">
-       <div className="b_profile_card">
-        <ProfileCard
-          jobs={userProfile.jobs}
-          gender={userProfile.gender}
-          userName={userProfile.userName}
-          career={userProfile.career}
-          point={userProfile.point}
-          intro={userProfile.intro}
-          stacks={userProfile.stacks}
-        />
-      </div>
-      <div className="b_right">
-        <div className="b_description">
-          <Description />
-          <div className="b_title_form">
-            <PostFrom />
+        <div className="b_profile_card">
+          <ProfileCard
+            jobs={ userProfile.jobs }
+            gender={ userProfile.gender }
+            userName={ userProfile.userName } 
+            career={ userProfile.career }
+            point={ userProfile.point }
+            intro={ userProfile.intro }
+            stacks={ userProfile.stacks }
+          />
+        </div>
+        <div className="b_right">
+          <div className="b_description">
+            <Description isEditMode={ isEditMode } />
+            <div className="b_title_form">
+              <PostForm isEditMode={ isEditMode } initialData={ initialData } />
+            </div>
           </div>
         </div>
       </div>
-     </div>
     </div>
   );
 }
-  
-  export default Board;
+
+export default CUBoard;
