a1466cc3d498dad056efc16684dda0be8cffbb55
minjaejeo <157557014+minjaejeo@users.noreply.github.com>
2024-07-01 16:41:57 +0900

    pickzoneDetail에 들어가는 resume 생성

--- frontend/ziczone/src/pickzone/components/PickzoneDetail.jsx
+++ frontend/ziczone/src/pickzone/components/PickzoneDetail.jsx
@@ -2,39 +2,72 @@ import React, {useEffect, useState} from "react";
 import { useParams } from "react-router";
 import axios from "axios";
 import ProfileCard from "../../common/card/components/ProfileCard";
+import Resume from "./Resume";
 import personalMImage from "../../common/card/assets/personal_m_image.png";
 import personalFImage from '../../common/card/assets/personal_f_image.png';
 
 export default function PickZoneDetail() {
     const { personalId} = useParams();
-    const [userDetails, setUserDetails] = useState(null);
+    // 회원 정보 담는 hook
+    const [userCard, setuserCard] = useState(null);
+    // resume 정보 담는 hook
+    const[userResume, setUserResume] = useState(null);
 
     useEffect(() => {
+        // pickDetail에서 왼쪽에 회원정보 가져오는 axios
         axios.get(`/api/pickcards/${personalId}`)
             .then(response => {
-                setUserDetails(response.data);
+                setuserCard(response.data);
             })
             .catch(error => {
-                console.log('Error fetching user detauls: ', error);
+                console.log('Error fetching user details: ', error);
+            });
+            // pickDetail에서 resume 데이터 가져오는 axios
+        axios.get(`/api/pickresume/${personalId}`)
+            .then(response => {
+                setUserResume(response.data);
+            })
+            .catch(error => {
+                console.log('Error fetching user resume details: ', error )
             });
     }, [personalId]);
-    if(!userDetails) {
+    // 2개 api 같이 가져올 때 밑에 구문 작성해야 rendering 할때 같이 가져와진다.
+    if(!userCard || !userResume) {
         return <div>Loading...</div>
     }
-    const userImage = userDetails.gender === 'MALE' ? personalMImage : personalFImage;
-    const jobNames = userDetails.jobName ? userDetails.jobName.split(',') : [];
-    const techNames = userDetails.techName ? userDetails.techName.split(',') : [];
+    // 성별에 따라 image다르게 보여주기
+    const userImage = userCard.gender === 'MALE' ? personalMImage : personalFImage;
+    // 직무 가져오기
+    const jobNames = userCard.jobName ? userCard.jobName.split(',') : [];
+    // 기술 가져오기
+    const techNames = userCard.techName ? userCard.techName.split(',') : [];
     return(
-        <ProfileCard
-            key={userDetails.personalId}
-            personalId={userDetails.personalId}
-            userImage={userImage}
-            jobNames={jobNames}
-            userName={userDetails.userName}
-            userCareer={userDetails.personalCareer}
-            userIntro={userDetails.userIntro}
-            techNames={techNames}
-        />
+        <div>
+            {/* pickDetail에서 회원정보 */}
+            <ProfileCard
+                key={userCard.personalId}
+                personalId={userCard.personalId}
+                userImage={userImage}
+                jobNames={jobNames}
+                userName={userCard.userName}
+                userCareer={userCard.personalCareer}
+                userIntro={userCard.userIntro}
+                techNames={techNames}
+            />
+            {/* pickDetail에서 resume 정보 */}
+            <Resume 
+                resumeName={userResume.resumeName}
+                phoneNum={userResume.phoneNum}
+                resumeDate={userResume.resumeDate}
+                jobName={jobNames}
+                techName={techNames}
+                educations={userResume.educations}
+                careers={userResume.careers}
+                curriculums={userResume.curriculums}
+                etcs={userResume.etcs}
+                archives={userResume.archives}
+            />
+        </div>
 
-    )
+    );
 }
