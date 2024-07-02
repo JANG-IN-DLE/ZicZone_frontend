feb64ad4aacedcebefa9df9d59bf6c3ead380f35
minjaejeo <157557014+minjaejeo@users.noreply.github.com>
2024-07-02 12:26:43 +0900

    dto에서 portfolio 넘오는 갯수에 맞게 portfolio1, portfolio2, ... 추가하여 해당 포트폴리오 표현

--- frontend/ziczone/src/pickzone/components/PickzoneDetail.jsx
+++ frontend/ziczone/src/pickzone/components/PickzoneDetail.jsx
@@ -3,6 +3,8 @@ import { useParams } from "react-router";
 import axios from "axios";
 import ProfileCard from "../../common/card/components/ProfileCard";
 import Resume from "./Resume";
+import CoverLetter from "./CoverLetter";
+import Portfolio from "./Portfolio";
 import personalMImage from "../../common/card/assets/personal_m_image.png";
 import personalFImage from '../../common/card/assets/personal_f_image.png';
 
@@ -12,6 +14,9 @@ export default function PickZoneDetail() {
     const [userCard, setuserCard] = useState(null);
     // resume 정보 담는 hook
     const[userResume, setUserResume] = useState(null);
+    // 선택 섹션 추적hook
+    const[selectedSection, setSelectedSection] = useState("resume");
+
 
     useEffect(() => {
         // pickDetail에서 왼쪽에 회원정보 가져오는 axios
@@ -41,8 +46,41 @@ export default function PickZoneDetail() {
     const jobNames = userCard.jobName ? userCard.jobName.split(',') : [];
     // 기술 가져오기
     const techNames = userCard.techName ? userCard.techName.split(',') : [];
+    
+    const renderSection = () => {
+        switch(selectedSection){
+            case 'resume':
+                return(
+                    // pickDetail에서 resume 정보
+                    <Resume 
+                    resumeName={userResume.resumeName}
+                    phoneNum={userResume.phoneNum}
+                    resumeDate={userResume.resumeDate}
+                    jobName={jobNames}
+                    techName={techNames}
+                    educations={userResume.educations}
+                    careers={userResume.careers}
+                    curriculums={userResume.curriculums}
+                    etcs={userResume.etcs}
+                    archives={userResume.archives}
+                />
+                );
+            case 'coverLetter':
+                return <CoverLetter data={userResume.personalState} />;
+            default:
+                // li에서 저장된 selectedSection이 portfolio로 시작하는 값을 누르면 
+                // 해당 userResume.portfolios로 넘어오는 값을 return <Portfolio/>
+                if(selectedSection.startsWith('portfolio')){
+                    const index = parseInt(selectedSection.replace('portfolio', ''))-1;
+                    return <Portfolio data={userResume.portfolios[index]} />;
+                }
+                return null;
+        }
+    };
+
     return(
-        <div>
+        <div style={{display: 'flex'}}>
+            <div style={{ marginRight: '20px' }}>
             {/* pickDetail에서 회원정보 */}
             <ProfileCard
                 key={userCard.personalId}
@@ -54,20 +92,23 @@ export default function PickZoneDetail() {
                 userIntro={userCard.userIntro}
                 techNames={techNames}
             />
-            {/* pickDetail에서 resume 정보 */}
-            <Resume 
-                resumeName={userResume.resumeName}
-                phoneNum={userResume.phoneNum}
-                resumeDate={userResume.resumeDate}
-                jobName={jobNames}
-                techName={techNames}
-                educations={userResume.educations}
-                careers={userResume.careers}
-                curriculums={userResume.curriculums}
-                etcs={userResume.etcs}
-                archives={userResume.archives}
-            />
+            <ul>
+                <li onClick={() => setSelectedSection('resume')}>이력서</li>
+                <li onClick={() => setSelectedSection('coverLetter')}>자기소개서</li>
+                {/* porfolio로 넘어오는 갯수에 맞게 li 정렬 */}
+                {userResume.portfolios && userResume.portfolios.map((portfolio, index)=>(
+                    <li key={index} onClick={() => setSelectedSection(`portfolio${index + 1}`)}>
+                        포트폴리오{index + 1}
+                    </li>
+                ))}
+            </ul>
+            
+            </div>
+            <div>
+                {renderSection()}
+            </div>
         </div>
 
     );
 }
+
