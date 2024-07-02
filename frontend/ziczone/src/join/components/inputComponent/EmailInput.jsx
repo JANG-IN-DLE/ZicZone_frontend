05ad3ad18062f53878db3811bf0b6a17a95b294a
Gaannini <0107162a@gmail.com>
2024-07-02 11:53:08 +0900

    이메일 인증 구현

--- frontend/ziczone/src/join/components/inputComponent/EmailInput.jsx
+++ frontend/ziczone/src/join/components/inputComponent/EmailInput.jsx
@@ -1,22 +1,60 @@
 import React from 'react';
-import "../../styles/JoinCom/EmailInput.css"
+import useEmailVerification from '../../hooks/useEmailAuth';
+import "../../styles/JoinCom/EmailInput.css";
 
 const EmailInput = () => {
+    const {
+        email,
+        inputCode,
+        isSend,
+        isAuth,
+        handleEmailChange,
+        handleCodeChange,
+        sendVerificationEmail,
+        verifyAuthCode
+    } = useEmailVerification();
+
     return (
         <div className="inputform email">
             <p>이메일</p>
             <div className="email_box">
                 <div className="email_input_box">
-                    <input type="email" />
-                    {/*  className="seccess" */}
-                    <button>인증번호 받기</button>
+                    <input 
+                        type="email"
+                        value={email}
+                        onChange={handleEmailChange}
+                        readOnly={isAuth === "auth_success" ? true : false}
+                        placeholder="이메일을 입력하세요"
+                    />
+                    <button 
+                        onClick={sendVerificationEmail} 
+                        className={isAuth === "auth_success" ? "success" : ""} 
+                        disabled={isAuth === "auth_success" ? true : false}
+                        style={isAuth === "auth_success" ? { cursor : 'default' } : null}>
+                        {isAuth === "auth_success" ? "인증 완료" : "인증번호 받기"}
+                    </button>
+                    <p className='senderror'>
+                        {isSend === "send_fail" ? "이메일 전송에 실패하였습니다" : isSend === "send_error" ? "오류가 발생했습니다" : "" }
+                    </p>
                 </div>
-                <div className="email_auth_box">
-                    <p>인증번호</p>
-                    <input type="text" />
-                    <button className="auth_btn">인증</button>
-                    <p className="wrongtext">인증번호가 일치하지 않습니다.</p>
-                </div>
+                    <div className={`email_auth_box ${isSend === "send_success" ? 'visible': ''}`}>
+                        <p>인증번호</p>
+                        <input 
+                            type="text" 
+                            value={inputCode}
+                            onChange={handleCodeChange}
+                            readOnly={isAuth === "auth_success" ? true : false}
+                            placeholder="인증번호를 입력하세요"
+                        />
+                        <button 
+                            className="auth_btn" 
+                            onClick={verifyAuthCode} 
+                            disabled={isAuth === "auth_success" ? true : false}
+                            style={isAuth === "auth_success" ? { cursor : 'default' } : null}>인증</button>
+                        <p className={`auth_msg ${isAuth === "" ? '': isAuth === "auth_success" ? 'auth_success_msg': 'auth_fail_msg'}`}>
+                            {isAuth === "auth_success" ? "인증 성공" : "인증 실패" }
+                        </p>
+                    </div>
             </div>
         </div>
     );
