1e50d3a85fa1205f04ea7b787db3e9afc085d52a
CHAERIM LEE <leechaerim214@gmail.com>
2024-07-09 11:51:30 +0900

    feat: 등록된 게시물이 없을 때 처리

--- frontend/ziczone/src/helpzone/components/BoardList.jsx
+++ frontend/ziczone/src/helpzone/components/BoardList.jsx
@@ -3,6 +3,10 @@ import "../styles/BoardList.css";
 import BoardItem from "./BoardItem";
 
 const BoardList = ({ boards }) => {
+  if (!boards || boards.length === 0) {
+    return <p className='bl_none'>등록된 게시물이 없습니다.</p>;
+  }
+
   return (
     <div className='board_list'>
       { boards.map(board => (
