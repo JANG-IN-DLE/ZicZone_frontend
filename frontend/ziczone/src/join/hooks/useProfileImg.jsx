2bd13b67913d50f95993af3ff58a28f9740d7bb3
Gaannini <0107162a@gmail.com>
2024-07-09 14:03:33 +0900

    회사로고 서(파일)버에 따로 전송

--- frontend/ziczone/src/join/hooks/useProfileImg.jsx
+++ frontend/ziczone/src/join/hooks/useProfileImg.jsx
@@ -2,8 +2,8 @@ import { useState, useRef } from 'react';
 import { useFormContext } from '../components/FormContext';
 
 const useProfileImage = () => {
-    const { updateFormData } = useFormContext(); 
-    const [profile, setProfile] = useState(null); //선택된 이미지의 url을 저장
+    const { formData, updateFormData } = useFormContext(); 
+    const [ profile, setProfile ] = useState(null); //선택된 이미지의 url을 저장
     const fileInputRef = useRef(null); //파일 입력 요소에 접근
 
     const handleCameraClick = () => {
@@ -13,13 +13,10 @@ const useProfileImage = () => {
     const handleFileChange = (event) => {
         const file = event.target.files[0]; //선택된 파일에 접근
         if (file) {
-            const reader = new FileReader(); //파일이 존재하면 FileReader객체 생성
-            reader.onloadend = () => {
-                const imageUrl = reader.result; //이미지 url저장
-                setProfile(reader.result);
-                updateFormData('companyLogo', imageUrl); //저장된 url profile 필드에 저장
-            };
-            reader.readAsDataURL(file);
+            const imageUrl = URL.createObjectURL(file);
+            setProfile(imageUrl);
+            updateFormData('companyLogo', file); // 파일 자체를 저장
+            console.log(formData);
         }
     };
 
