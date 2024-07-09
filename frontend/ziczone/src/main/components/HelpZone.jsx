6c8b862bf66fc7a601453d3fb2b8aa68be5e82ba
myumyuzzang <myu.myu.zzang@gmail.com>
2024-07-09 11:40:56 +0900

    메인페이지 헬프존 데이터 불러오기

--- frontend/ziczone/src/main/components/HelpZone.jsx
+++ frontend/ziczone/src/main/components/HelpZone.jsx
@@ -1,16 +1,42 @@
 import React from "react";
 import { useNavigate } from "react-router";
 
-const HelpZone = ({ corrModify, corrPoint, corrTitle, corrView, userId }) => {
+const HelpZone = ({
+  corrModify,
+  corrPoint,
+  corrTitle,
+  corrView,
+  userId,
+  personalCareer,
+  userName,
+}) => {
+  const getPointStyle = (point) => {
+    switch (point) {
+      case 100:
+        return { backgroundColor: "#FFFFFF", color: "#000000" };
+      case 200:
+        return { backgroundColor: "rgba(0, 81, 186, 0.25)", color: "#FFFFFF" };
+      case 500:
+        return { backgroundColor: "rgba(0, 81, 186, 0.5)", color: "#FFFFFF" };
+      case 1000:
+        return { backgroundColor: "rgba(0, 81, 186, 0.75)", color: "#FFFFFF" };
+      case 1500:
+        return { backgroundColor: "#0051BA", color: "#FFFFFF" };
+      default:
+        return { backgroundColor: "#FFFFFF", color: "#000000" };
+    }
+  };
   return (
     <>
       <div className="help_list">
-        <div className="help_list_berry">{corrPoint}</div>
+        <div className="help_list_berry" style={getPointStyle(corrPoint)}>
+          {corrPoint}
+        </div>
         <div className="help_list_content">
           <div className="help_list_title">{corrTitle}</div>
           <div className="help_list_user_name">
-            {userId}
-            {/* <span className="help_list_user_career"> | {userCareer}</span> */}
+            {userName}
+            <span className="help_list_user_career"> | {personalCareer}</span>
           </div>
         </div>
         <div className="help_list_date">
