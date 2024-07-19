cde93233a67d7322bf545f8387825336296c357c
KangHyun_Kim <162311939+hmmumm@users.noreply.github.com>
2024-07-19 15:30:26 +0900

    Toss Pay 프론트 간단 구현

---
+++ frontend/ziczone/src/mypage/components/Toss/CheckoutPage.jsx
@@ -0,0 +1,79 @@
+
+import { useEffect, useState } from "react";
+import { loadTossPayments, ANONYMOUS } from "@tosspayments/tosspayments-sdk";
+import '../../styles/Toss.css';
+
+const generateRandomString = () => window.btoa(Math.random()).slice(0, 20);
+const clientKey = "test_gck_docs_Ovk5rk1EwkEbP0W43n07xlzm";
+
+export default function CheckoutPage(money) {
+  money = 1000;
+  const amountMoney = money;
+  const [ready, setReady] = useState(false);
+  const [widgets, setWidgets] = useState(null);
+  const [amount, setAmount] = useState({
+    currency: "KRW",
+    value: amountMoney,
+  });
+
+  useEffect(() => {
+    async function fetchPaymentWidgets() {
+      const tossPayments = await loadTossPayments(clientKey);
+      const widgets = tossPayments.widgets({ customerKey: ANONYMOUS });
+      setWidgets(widgets);
+    }
+
+    fetchPaymentWidgets();
+  }, [clientKey]);
+
+  useEffect(() => {
+    async function renderPaymentWidgets() {
+      if (widgets == null) {
+        return;
+      }
+      await widgets.setAmount(amount);
+      await widgets.renderPaymentMethods({
+        selector: "#payment-method",
+        variantKey: "DEFAULT",
+      });
+      await widgets.renderAgreement({
+        selector: "#agreement",
+        variantKey: "AGREEMENT",
+      });
+
+      setReady(true);
+    }
+
+    renderPaymentWidgets();
+  }, [widgets]);
+
+  return (
+    <div className="wrapper w-100">
+      <div className="max-w-540 w-100">
+        <div id="payment-method" className="w-100" />
+        <div id="agreement" className="w-100" />
+        <div className="btn-wrapper w-100">
+          <button
+            className="btn primary w-100"
+            onClick={async () => {
+              try {
+                await widgets?.requestPayment({
+                  orderId: generateRandomString(),
+                  orderName: "토스 티셔츠 외 2건",
+                  customerName: "김토스",
+                  customerEmail: "customer123@gmail.com",
+                  successUrl: window.location.origin + "/success" + window.location.search,
+                  failUrl: window.location.origin + "/fail" + window.location.search
+                });
+              } catch (error) {
+                // TODO: 에러 처리
+              }
+            }}
+          >
+            결제하기
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
