134b4ed0aa69bd1126f20170f3a1495951d8e520
kangseunggyuu <157556960+kangseunggyuu@users.noreply.github.com>
2024-07-25 17:02:27 +0900

    마이페이지 데이터 렌더링 전 Loading 띄우기

--- frontend/ziczone/src/mypage/components/MypageTop/MypageTop.jsx
+++ frontend/ziczone/src/mypage/components/MypageTop/MypageTop.jsx
@@ -1,27 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './../../styles/MypageTop.css'
 import MypageUserModal from "../MypageModal/MypageUserModal";
 import CheckPassword from "../MypageModal/CheckPassword";
 import useModal from "../../hooks/useEditModal";
-import maleImg from "../../../common/card/assets/personal_m_image.png"
-import femaleImg from "../../../common/card/assets/personal_f_image.png"
+import maleImg from "../../../common/card/assets/personal_m_image.png";
+import femaleImg from "../../../common/card/assets/personal_f_image.png";
 
 const MypageTop = ({ gender }) => {
     const { isPasswordModalOpen, handleOpenPasswordModalOpen, handleClosePasswordModalOpen, isEditModalOpen, handleOpenEditModalOpen, handleCloseEditModalOpen } = useModal();
+    const [genderImg, setGenderImg] = useState(null); // 초기 상태를 null로 설정
 
-    const genderImg = gender === 'MALE' ? maleImg : femaleImg;
+    useEffect(() => {
+        if (gender) { // gender가 유효한 경우에만 실행
+            // 데이터 로드 후 성별에 따라 이미지를 설정
+            if (gender === 'FEMALE') {
+                setGenderImg(femaleImg);
+            } else if (gender === 'MALE') {
+                setGenderImg(maleImg);
+            }
+        }
+    }, [gender]); // gender가 변경될 때마다 실행
 
     const handlePasswordSuccess = () => {
         handleClosePasswordModalOpen();
         handleOpenEditModalOpen();
-    }
+    };
 
     return (
         <div className="mypage_top">
             <span className="mypage">마이페이지</span>
             <div className="mypage_top_bar">
                 <div className="mypage_top_circle">
-                    <img src={genderImg} alt="{gender}" />
+                    {genderImg ? (
+                        <img src={genderImg} alt={gender} /> // 데이터가 로드된 후에만 이미지 표시
+                    ) : (
+                        <div>Loading...</div> // 데이터 로드 전 로딩 문구 표시
+                    )}
                 </div>
             </div>
             <div className="mypage_top_edit">
